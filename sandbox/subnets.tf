de6f243b9ec9311056cec9470da6638d2528a7f0
Mark James <mark.james@airwalkconsulting.com>
2020-04-16 09:02:17 +0100

    .

--- sandbox/subnets.tf
+++ sandbox/subnets.tf
@@ -1,7 +1,7 @@
 # Define the Public Subnets
 resource "aws_subnet" "public_subnet1" {
   vpc_id = aws_vpc.default.id
-  cidr_block = "$(var.public_subnet_1_cidr)"
+  cidr_block = var.public_subnet1_cidr
   availability_zone = "us-east-1a"
   depends_on = [aws_vpc.default]
 
@@ -12,7 +12,7 @@ resource "aws_subnet" "public_subnet1" {
 
 resource "aws_subnet" "public_subnet2" {
   vpc_id = aws_vpc.default.id
-  cidr_block = "$(var.public_subnet_2_cidr)"
+  cidr_block = var.public_subnet2_cidr
   availability_zone = "us-east-1b"
   depends_on = [aws_vpc.default]
 
@@ -24,7 +24,7 @@ resource "aws_subnet" "public_subnet2" {
 # Define the Private Subnets
 resource "aws_subnet" "private_subnet1" {
   vpc_id = aws_vpc.default.id
-  cidr_block = "$(var.private_subnet_1_cidr)"
+  cidr_block = var.private_subnet1_cidr
   availability_zone = "us-east-1a"
   depends_on = [aws_vpc.default]
 
@@ -35,7 +35,7 @@ resource "aws_subnet" "private_subnet1" {
 
 resource "aws_subnet" "private_subnet2" {
   vpc_id = aws_vpc.default.id
-  cidr_block = "$(var.private_subnet_2_cidr)"
+  cidr_block = var.private_subnet2_cidr
   availability_zone = "us-east-1b"
   depends_on = [aws_vpc.default]
 
@@ -47,7 +47,7 @@ resource "aws_subnet" "private_subnet2" {
 # Define the Database Subnets
 resource "aws_subnet" "private_database_subnet1" {
   vpc_id = aws_vpc.default.id
-  cidr_block = "$(var.private_subnet_1_cidr)"
+  cidr_block = var.private_subnet1_cidr
   availability_zone = "us-east-1a"
   depends_on = [aws_vpc.default]
 
@@ -58,7 +58,7 @@ resource "aws_subnet" "private_database_subnet1" {
 
 resource "aws_subnet" "private_database_subnet2" {
   vpc_id = aws_vpc.default.id
-  cidr_block = "$(var.private_subnet_2_cidr)"
+  cidr_block = var.private_subnet2_cidr
   availability_zone = "us-east-1b"
   depends_on = [aws_vpc.default]
 
@@ -70,7 +70,7 @@ resource "aws_subnet" "private_database_subnet2" {
 # Define the Database Subnet Group
 resource "aws_db_subnet_group" "default" {
   name = "db-subnet-group"
-  subnet_ids = ["${aws_subnet.private_database_subnet1.id}","${aws_subnet.private_database_subnet2.id}"]
+  subnet_ids = [aws_subnet.private_database_subnet1.id, aws_subnet.private_database_subnet2.id]
   depends_on = [aws_subnet.private_database_subnet1, aws_subnet.private_database_subnet2]
 
   tags = {
