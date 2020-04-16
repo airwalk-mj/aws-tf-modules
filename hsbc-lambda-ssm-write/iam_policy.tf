a55c7de1474a8134f218031ecebf666d570d440c
Mark James <mark.james@airwalkconsulting.com>
2020-04-16 18:16:34 +0100

    .

--- hsbc-lambda-ssm-write/iam_policy.tf
+++ hsbc-lambda-ssm-write/iam_policy.tf
@@ -3,7 +3,7 @@
 resource "aws_iam_policy" "logging-policy" {
   name        = "lambda-logging-policy"
   description = "A test policy to allow lambda to access the Xray"
-  depends_on  = [aws_iam_role.iam_for_lambda]
+  depends_on  = [aws_iam_role.lambda-ssm_write]
   policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -26,7 +26,7 @@ EOF
 resource "aws_iam_policy" "ssm-policy_write" {
   name        = "lambda-ssm-policy"
   description = "A test policy to allow lambda to access secrets manager"
-  depends_on  = [aws_iam_role.iam_for_lambda]
+  depends_on  = [aws_iam_role.lambda-ssm_write]
   policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -49,7 +49,7 @@ EOF
 resource "aws_iam_policy" "kms-policy_write" {
   name        = "lambda-kms-policy"
   description = "A test policy to allow lambda to access the KMS so it can decrypt using CMK"
-  depends_on  = [aws_iam_role.iam_for_lambda]
+  depends_on  = [aws_iam_role.lambda-ssm_write]
   policy = <<EOF
 {
   "Version": "2012-10-17",
