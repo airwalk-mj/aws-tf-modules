22699e2e963187f238787cd4688df8d6acd4b3c6
Mark James <mark.james@airwalkconsulting.com>
2020-04-16 16:25:43 +0100

    .

--- hsbc-lambda-ssm-test/iam_policy.tf
+++ hsbc-lambda-ssm-test/iam_policy.tf
@@ -9,11 +9,11 @@ resource "aws_iam_policy" "ssm-policy" {
   "Statement": [
     {
       "Action": [
-        "secretsmanager:GetSecretValue",
-        "ssm:GetParameter*"
+        "ssm:GetParameters",
+        "secretsmanager:GetSecretValue*"
       ],
       "Effect": "Allow",
-      "Resource": "arn:aws:ssm:us-east-1:544294979223:parameter/*"
+      "Resource": "*"
     }
   ]
 }
@@ -34,7 +34,7 @@ resource "aws_iam_policy" "acm-policy" {
         "acm:GetCertificate"
       ],
       "Effect": "Allow",
-      "Resource": "arn:aws:kms:us-east-1:544294979223:*"
+      "Resource": "*"
     }
   ]
 }
@@ -50,7 +50,6 @@ resource "aws_iam_policy" "logging-policy" {
 {
   "Version": "2012-10-17",
   "Statement": {
-      "Effect": "Allow",
       "Action": [
           "xray:PutTraceSegments",
           "xray:PutTelemetryRecords",
@@ -58,14 +57,12 @@ resource "aws_iam_policy" "logging-policy" {
           "logs:CreateLogStream",
           "logs:PutLogEvents"
       ],
-      "Resource": [
-          "*"
-      ]
+      "Effect": "Allow",
+      "Resource": "*"
   }
 }
 EOF
 }
-
 
 # Attach this policy to the IAM role
 resource "aws_iam_role_policy_attachment" "attach1" {
@@ -80,7 +77,34 @@ resource "aws_iam_role_policy_attachment" "attach2" {
 }
 
 # Attach this policy to the IAM role
-resource "aws_iam_role_policy_attachment" "attach4" {
+resource "aws_iam_role_policy_attachment" "attach3" {
   role       = aws_iam_role.iam_for_lambda.name
   policy_arn = aws_iam_policy.logging-policy.arn
+}
+
+resource "aws_iam_policy" "kms-policy" {
+  name        = "lambda-kms-policy"
+  description = "A test policy to allow lambda to access the KMS so it can decrypt using CMK"
+  depends_on  = [aws_iam_role.iam_for_lambda]
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": {
+    "Effect": "Allow",
+    "Action": [
+      "kms:DescribeKey",
+      "kms:GenerateDataKey",
+      "kms:Decrypt"
+    ],
+    "Resource": [
+      "arn:aws:kms:us-east-1:544294979223:key/20a7ba6d-7b62-460d-a9ec-6aba8c9cde58"
+    ]
+  }
+}
+EOF
+}
+# Attach this policy to the IAM role
+resource "aws_iam_role_policy_attachment" "attach4" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.kms-policy.arn
 }
