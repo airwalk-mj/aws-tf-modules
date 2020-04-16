cbef16c119e7177044435c24c0e63ffd6de3d41d
Mark James <mark.james@airwalkconsulting.com>
2020-04-16 13:23:10 +0100

    .

--- hsbc-lambda-ssm-test/iam_policy.tf
+++ hsbc-lambda-ssm-test/iam_policy.tf
@@ -1,7 +1,7 @@
 
-resource "aws_iam_policy" "policy" {
-  name        = "test-policy"
-  description = "A test policy"
+resource "aws_iam_policy" "ssm-policy" {
+  name        = "lambda-ssm-policy"
+  description = "A test policy to allow lambda to access secrets manager"
   depends_on  = [aws_iam_role.iam_for_lambda]
   policy = <<EOF
 {
@@ -18,9 +18,34 @@ resource "aws_iam_policy" "policy" {
 }
 EOF
 }
-
 # Attach this policy to the IAM role
-resource "aws_iam_role_policy_attachment" "attach" {
+resource "aws_iam_role_policy_attachment" "attach1" {
   role       = aws_iam_role.iam_for_lambda.name
-  policy_arn = aws_iam_policy.policy.arn
+  policy_arn = aws_iam_policy.ssm-policy.arn
+}
+
+
+resource "aws_iam_policy" "kms-policy" {
+  name        = "lambda-kms-policy"
+  description = "A test policy to allow lambda to access the KMS keys"
+  depends_on  = [aws_iam_role.iam_for_lambda]
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ssm:GetParameter*"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:kms:us-east-1:544294979223:*"
+    }
+  ]
+}
+EOF
+}
+# Attach this policy to the IAM role
+resource "aws_iam_role_policy_attachment" "attach2" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.kms-policy.arn
 }
