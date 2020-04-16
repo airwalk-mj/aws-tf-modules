6123a79d5c967d0a83536e9ca5b5851353601f52
Mark James <mark.james@airwalkconsulting.com>
2020-04-16 18:18:24 +0100

    .

--- hsbc-lambda-ssm-read/lambda.tf
+++ hsbc-lambda-ssm-read/lambda.tf
@@ -12,9 +12,9 @@ data "archive_file" "lambda_zip" {
 resource "aws_lambda_function" "test_lambda" {
   filename         = "lambda_function.zip"
   function_name    = "test_lambda"
-  role             = aws_iam_role.iam_for_lambda.arn
+  role             = aws_iam_role.lambda-ssm_read.arn
   handler          = "lambda.get_secret"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   runtime          = "python3.6"
-  depends_on       = [aws_iam_role.iam_for_lambda]
+  depends_on       = [aws_iam_role.lambda-ssm_read]
 }
