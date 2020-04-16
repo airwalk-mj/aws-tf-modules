62fd446bb6e713f6ba3dbc7240ef0f949572ad19
Mark James <mark.james@airwalkconsulting.com>
2020-04-16 17:55:32 +0100

    .

--- hsbc-lambda-ssm-write/kms.tf
+++ hsbc-lambda-ssm-write/kms.tf
@@ -1,10 +1,10 @@
 
 resource "aws_kms_key" "test" {
-  description             = "kms test key"
+  description             = "kms test key2"
   deletion_window_in_days = 10
 }
 
 resource "aws_kms_alias" "test" {
-  name          = "alias/mule/policy/secret_key"
+  name          = "alias/mule/policy/secret_key2"
   target_key_id = aws_kms_key.test.key_id
 }
--- hsbc-lambda-ssm-write/lambda.tf
+++ hsbc-lambda-ssm-write/lambda.tf
@@ -12,7 +12,7 @@ data "archive_file" "lambda_zip" {
 resource "aws_lambda_function" "test_lambda" {
   filename         = "lambda_function.zip"
   function_name    = "test_lambda"
-  role             = aws_iam_role.iam_for_lambda.arn
+  role             = aws_iam_role.lambda-ssm_write.arn
   handler          = "lambda.get_secret"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   runtime          = "python3.6"
