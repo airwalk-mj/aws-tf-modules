e99a11475f09ca519058635851d21a602293bc82
Mark James <mark.james@airwalkconsulting.com>
2020-04-16 11:35:46 +0100

    .

---
+++ hsbc-lambda-ssm-test/iam_policy.tf
@@ -0,0 +1,27 @@
+
+resource "aws_iam_policy" "policy" {
+  name        = "test-policy"
+  description = "A test policy"
+  dependancy  = [aws_iam_role.iam_for_lambda]
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ssm:GetParameter*"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:ssm:us-east-1:544294979223:parameter/*"
+    }
+  ]
+}
+EOF
+}
+
+# Attach this policy to the IAM role
+resource "aws_iam_role_policy_attachment" "attach" {
+  role       = aws_iam_role.iam_for_lambda
+  policy_arn = aws_iam_policy.policy.arn
+}
--- hsbc-lambda-ssm-test/secrets-manager.tf
+++ hsbc-lambda-ssm-test/secrets-manager.tf
@@ -1,5 +1,6 @@
 
 resource "aws_secretsmanager_secret" "example" {
   name = "example"
+  kms_key_id = aws_kms_key.test.id
   depends_on = [aws_kms_key.test]
 }
