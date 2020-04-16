0500aabd1b92b712af4019514f9953640b88eb32
Mark James <mark.james@airwalkconsulting.com>
2020-04-16 07:16:27 +0100

    .

---
+++ lambda-ssm-get-secret/python/build_env.sh
@@ -0,0 +1,7 @@
+export VIRTUALENV='venv_lambda'
+rm -fr $VIRTUALENV
+# Setup fresh virtualenv and install requirements
+virtualenv $VIRTUALENV
+source $VIRTUALENV/bin/activate
+pip install -r requirements.txt
+deactivate
