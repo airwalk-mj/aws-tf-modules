d632835059d60e4a8c2f175960da3db72e3f5a9a
Mark James <mark.james@airwalkconsulting.com>
2020-04-16 09:44:03 +0100

    .

--- hsbc-lambda-ssm-test/lambda.tf
+++ hsbc-lambda-ssm-test/lambda.tf
@@ -1,16 +1,39 @@
-# Define the VPC
-resource "aws_lambda" "default" {
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
 
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 }
 
-# Define the Public Subnets
-resource "aws_subnet" "public_subnet1" {
-  vpc_id = aws_vpc.default.id
-  cidr_block = var.public_subnet1_cidr
-  availability_zone = "us-east-1a"
-  depends_on = [aws_vpc.default]
+resource "aws_lambda_function" "test_lambda" {
+  filename      = "lambda_function_payload.zip"
+  function_name = "lambda_function_name"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "exports.test"
 
-  tags = {
-    name = "Public Subnet 1"
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  source_code_hash = filebase64sha256("lambda_function_payload.zip")
+
+  runtime = "nodejs12.x"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
   }
 }
