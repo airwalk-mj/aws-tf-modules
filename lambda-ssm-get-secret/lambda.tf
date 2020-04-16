ef654829dd30a2323ad7099e92a6d899ba19113e
Mark James <mark.james@airwalkconsulting.com>
2020-04-16 07:29:44 +0100

    .

--- lambda-ssm-get-secret/lambda.tf
+++ lambda-ssm-get-secret/lambda.tf
@@ -19,7 +19,7 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 
-resource "aws_lambda_function" "test_lambda" {
+resource "aws_lambda_function" "lambda" {
   filename      = "lambda_function_payload.zip"
   function_name = "lambda_function_name"
   role          = aws_iam_role.iam_for_lambda.arn
@@ -28,7 +28,7 @@ resource "aws_lambda_function" "test_lambda" {
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  source_code_hash = "${base64sha256(file("./lambda_function_payload.zip"))}"
+  source_code_hash = base64sha256(file("lambda_function_payload.zip"))
 
   runtime = "nodejs12.x"
 
