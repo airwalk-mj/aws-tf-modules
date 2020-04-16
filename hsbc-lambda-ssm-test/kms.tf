78ad0f184be5f757817b37f86ff516b795f6aec7
Mark James <mark.james@airwalkconsulting.com>
2020-04-16 13:25:42 +0100

    .

--- hsbc-lambda-ssm-test/kms.tf
+++ hsbc-lambda-ssm-test/kms.tf
@@ -3,3 +3,8 @@ resource "aws_kms_key" "test" {
   description             = "kms test key"
   deletion_window_in_days = 10
 }
+
+resource "aws_kms_alias" "test" {
+  name          = "mule/policy/secret_key"
+  target_key_id = "${aws_kms_key.test.key_id}"
+}
