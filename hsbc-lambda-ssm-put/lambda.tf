0d710d0e31724161259b0921cfe0d3a729e06e7f
Mark James <mark.james@airwalkconsulting.com>
2020-04-17 08:56:54 +0100

    .

--- hsbc-lambda-ssm-put/lambda.tf
+++ hsbc-lambda-ssm-put/lambda.tf
@@ -12,9 +12,9 @@ data "archive_file" "lambda_zip" {
 resource "aws_lambda_function" "ssm-put-secret" {
   filename         = "lambda_function.zip"
   function_name    = "ssm-put-secret"
-  role             = aws_iam_role.lambda-ssm-write.arn
+  role             = aws_iam_role.lambda-ssm-put.arn
   handler          = "lambda.put_secret"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   runtime          = "python3.6"
-  depends_on       = [aws_iam_role.lambda-ssm-write]
+  depends_on       = [aws_iam_role.lambda-ssm-put]
 }
