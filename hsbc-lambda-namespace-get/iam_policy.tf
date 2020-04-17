071fe95cd8e1ca8b0f14643e8c361fc64c224b4a
Mark James <mark.james@airwalkconsulting.com>
2020-04-17 13:26:23 +0100

    .

---
+++ hsbc-lambda-namespace-get/iam_policy.tf
@@ -0,0 +1,23 @@
+
+resource "aws_iam_policy" "lambda-namespace-get-meta" {
+  name        = "lambda-namespace-get-meta-policy"
+  description = "A test policy to allow lambda to query a K*s namespace for metadata"
+  depends_on  = [aws_iam_role.lambda-namespace-get-meta]
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": {
+      "Action": [
+          "eks:*""
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+  }
+}
+EOF
+}
+# Attach this policy to the IAM role
+resource "aws_iam_role_policy_attachment" "attach1" {
+  role       = aws_iam_role.lambda-namespace-get-meta.name
+  policy_arn = aws_iam_policy.lambda-namespace-get-meta.arn
+}
---
+++ hsbc-lambda-namespace-get/lambda.tf
@@ -0,0 +1,20 @@
+# Simple AWS Lambda Terraform Example
+# requires 'python.py' in the same directory
+# to test: run `terraform plan`
+# to deploy: run `terraform apply`
+
+data "archive_file" "lambda_zip" {
+    type          = "zip"
+    source_file   = "lambda.py"
+    output_path   = "lambda_function.zip"
+}
+
+resource "aws_lambda_function" "lambda-k8s-namespace-get-meta" {
+  filename         = "lambda_function.zip"
+  function_name    = "lambda-k8s-namespace-get-meta"
+  role             = aws_iam_role.lambda-namespace-get-meta.arn
+  handler          = "lambda.get_secret"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  runtime          = "python3.6"
+  depends_on       = [aws_iam_role.lambda-namespace-get-meta]
+}
