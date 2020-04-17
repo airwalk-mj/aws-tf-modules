68da74561b5b13bf52f1a886e78f8dab5522a77c
Mark James <mark.james@airwalkconsulting.com>
2020-04-17 15:33:58 +0100

    .

--- hsbc-lambda-namespace-get/iam_policy.tf
+++ hsbc-lambda-namespace-get/iam_policy.tf
@@ -1,14 +1,15 @@
 
 resource "aws_iam_policy" "lambda-namespace-get-meta" {
   name        = "lambda-namespace-get-meta-policy"
-  description = "A test policy to allow lambda to query a K*s namespace for metadata"
+  description = "A test policy to allow lambda to query a K8s namespace for metadata"
   depends_on  = [aws_iam_role.lambda-namespace-get-meta]
   policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": {
       "Action": [
-          "eks:*""
+        "sts:GetCallerIdentity",
+        "eks:DescribeCluster",
       ],
       "Effect": "Allow",
       "Resource": "*"
